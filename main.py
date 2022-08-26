41d1ed2c89fb3fd212884c5d4f9683d3f41c122e
Czarny PC <dczarny10@gmail.com>
2022-08-26 10:56:41 +0200

    Total change of how data is stored - eliminated inefficient list searching Need to fully modify data func

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import PLM_BOM_transpose
 sg.theme("DarkTeal12")
 
 def PopupQuickView(subassembly):
-    if not any(subassembly[0] in i for i in sub_products): #check if subassembly is loaded. #any because products is list of tuples and need to iterate throught it
+    subassembly_revs = [i for i in data_subassemblies.keys() if subassembly[0] in i]
+    if not subassembly_revs:
         sg.Popup("No such subassembly loaded")
         return None
+    elif len(subassembly_revs) > 1:
+        sg.Popup("More than one subassembly with this number loaded")
+        return None
 
-    subassembly_parts = sorted([i[1:] for i in sub_parts if subassembly[0] in i[0]]) #create list of parts that are in picked subassembly. #i[1:] because first position in parts is product number which is not needed here
+    subassembly_parts = sorted(data_subassemblies[subassembly_revs[0]]) #create list of parts that are in picked subassembly
     #declaration for QuickView table
     window_q = sg.Window("QuickView",
                          [[sg.Table(values=subassembly_parts, headings=headings, max_col_width=700,
@@ -48,8 +52,11 @@ def compare(source):
             picked_right = values['-IN_RIGHT-'].replace("'", "").replace(",", "").replace("(", "").replace(")", "") #removes ',() from input
         else:
             picked_right = values['-BOX_RIGHT-'][0]
-        parts_right = sorted([i[1:] for i in parts if i[0] == picked_right]) #create list of parts that are in picked product
-        window['-TABLE_RIGHT-'].update(parts_right) #update Table
+        if picked_right in data.keys():
+            parts_right = sorted(data[picked_right])
+            window['-TABLE_RIGHT-'].update(parts_right) #update Table
+        else:
+            window['-TABLE_RIGHT-'].update([])  # if user erased input set table to empty
 
     else:
         window['-TABLE_RIGHT-'].update([]) #if user erased input set table to empty
@@ -59,8 +66,11 @@ def compare(source):
             picked_left = values['-IN_LEFT-'].replace("'", "").replace(",", "").replace("(", "").replace(")", "") #removes ',() from input
         else:
             picked_left = values['-BOX_LEFT-'][0]
-        parts_left = sorted([i[1:] for i in parts if i[0] == picked_left]) #create list of parts that are in picked product
-        window['-TABLE_LEFT-'].update(parts_left) #update Table
+        if picked_left in data.keys():
+            parts_left = sorted(data[picked_left])
+            window['-TABLE_LEFT-'].update(parts_left) #update Table
+        else:
+            window['-TABLE_LEFT-'].update([])  # if user erased input set table to empty
 
     else:
         window['-TABLE_LEFT-'].update([]) #if user erased input set table to empty
@@ -98,56 +108,62 @@ def compare(source):
         window['-TABLE_RIGHT-'].update(row_colors=((0, ''),))
 
 def expand(parts_to_expand, products_to_expand):
-    if not any(parts_to_expand[0] in i for i in sub_products): #check if subassembly is loaded. #any because products is list of tuples and need to iterate throught it
-        sg.Popup("No such subassembly loaded")
-        return None
-    if type(products_to_expand) == str: #if extend only one part for one product
-        products_to_expand = products_to_expand.replace("'", "").replace(",", "").replace("(", "").replace(")", "")
-        index = -1
-        for i, obj in enumerate(parts):  #enumerate through loaded parts
-            if obj[0] == products_to_expand:  #if picked product
-                if obj[1] == parts_to_expand[0]:  #if same part
+
+    if parts_to_expand == "all_subassemblies":
+        for o in data_subassemblies.keys():  # iterate through all subassembly products
+            subassembly_parts = data_subassemblies[o]
+            if o.endswith("_SAP"):  # strip subassembly product number of _SAP or plant and revision level
+                o = o[:-4]
+            else:
+                o = o[:-10]
+            for k in data.keys():  # iterate through all products
+                index = -1
+                for i, obj in enumerate(data[k]):  # enumerate through loaded parts
+                    if obj[0] == o:  # if same part
+                        index = i
+                        break
+                if index > -1:
+                    del data[k][index]  # delete original part
+                    data[k].extend([(i[0] + "_m", i[1], i[2]) for i in subassembly_parts])
+
+    else: #if extend only one part for all products #parts_to_expand == part number
+        subassembly_revs = [i for i in data_subassemblies.keys() if parts_to_expand[0] in i]
+        if not subassembly_revs:
+            sg.Popup("No such subassembly loaded")
+            return None
+        elif len(subassembly_revs) > 1:
+            sg.Popup("More than one subassembly with this part number loaded")
+            #add option to pick which one to use
+            return None
+        if products_to_expand == "all_products":
+            subassembly_parts = data_subassemblies[subassembly_revs[0]]
+            for k in data.keys(): #iterate through all products
+                index = -1
+                for i, obj in enumerate(data[k]):  # enumerate through loaded parts
+                    if obj[0] == parts_to_expand[0]:  # if same part
+                        index = i
+                        break
+                if index > -1:
+                    del data[k][index]  # delete original part
+                    data[k].extend([(i[0] + "_m", i[1], i[2]) for i in subassembly_parts])  # add subassembly parts to parts list
+
+        else:#if extend only one part for one product
+            subassembly_parts = data_subassemblies[subassembly_revs[0]]
+            products_to_expand = products_to_expand.replace("'", "").replace(",", "").replace("(", "").replace(")", "")
+            index = -1
+            for i, obj in enumerate(data[products_to_expand]):  # enumerate through loaded parts
+                if obj[0] == parts_to_expand[0]:  # if same part
                     index = i
                     break
-        subassembly_parts = sorted([i[1:] for i in sub_parts if parts_to_expand[0] in i[0]]) #create list with subassembly parts
-        del parts[index] #delete original part
-        parts.extend([(products_to_expand, i[0]+"_M", i[1], i[2]) for i in subassembly_parts]) #add subassembly parts to parts list
+            del data[products_to_expand][index] #delete original part
+            data[products_to_expand].extend([(i[0]+"_m", i[1], i[2]) for i in subassembly_parts]) #add subassembly parts to parts list
 
-    else:
-        if len(parts_to_expand) == 1: #if extend only one part for all products
-            for k in products_to_expand: #iterate through all products
-                index = -1
-                for i, obj in enumerate(parts):  # enumerate through loaded parts
-                    if obj[0] == k:  # if picked product
-                        if obj[1] == parts_to_expand[0]:  # if same part
-                            index = i
-                            break
-                if index > -1:
-                    subassembly_parts = sorted([i[1:] for i in sub_parts if parts_to_expand[0] in i[0]])  # create list with subassembly parts
-                    del parts[index]  # delete original part
-                    parts.extend([(k, i[0] + "_M", i[1], i[2]) for i in subassembly_parts])  # add subassembly parts to parts list
 
-        else:
-            for o in sub_products: #iterate through all subassembly products
-                if o.endswith("_SAP"): #strip subassembly product number of _SAP or plant and revision level
-                    o = o[:-4]
-                else:
-                    o = o[:-10]
-                for k in products_to_expand: #iterate through all products
-                    index = -1
-                    for i, obj in enumerate(parts):  #iterate through all loaded parts
-                        if obj[0] == k:  # if same product
-                            if obj[1] == o:  # if same part
-                                index = i
-                                break
-                    if index > -1: #if found same part in same product
-                        subassembly_parts = sorted([i[1:] for i in sub_parts if o in i[0]])  # create list with subassembly parts
-                        del parts[index]  # delete original part
-                        parts.extend([(k, i[0] + "_M", i[1], i[2]) for i in subassembly_parts])  # add subassembly parts to parts list
+
     return True
 
 def mass_check():
-    if len(products) < 1:
+    if len(data) < 1:
         sg.Popup("No parts loaded")
         return False
 
@@ -265,7 +281,6 @@ list_element_right: sg.Listbox = window.Element(
     '-BOX_RIGHT-')  # store listbox element for easier access and to get to docstrings
 prediction_list_left, input_text_left, sel_item_left = [], "", 0
 prediction_list_right, input_text_right, sel_item_right = [], "", 0
-products, parts, sub_products, sub_parts = [], [], [], []
 data = {}
 data_subassemblies = {}
 
@@ -281,12 +296,8 @@ while True:
         try:
             files = sg.PopupGetFile('Select folder', no_window=True, multiple_files=True,
                                     file_types=(("Excel files", [".xls", ".xlsx"]),))
-            data_t, products_t, parts_t = PLM_BOM_transpose.load_files(files)
-            products += products_t
-            parts += parts_t
+            data_t = PLM_BOM_transpose.load_files(files)
             data.update(data_t)
-            products = list(set(products))
-            parts = list(set(parts))
         except Exception as e:
             print(e)
             sg.Popup('Wrong file', keep_on_top=True)
@@ -295,12 +306,8 @@ while True:
         try:
             files = sg.PopupGetFile('Select folder', no_window=True, multiple_files=True,
                                     file_types=(("Excel files", [".xls", ".xlsx"]),))
-            data_subassemblies_t, sub_products_t, sub_parts_t = PLM_BOM_transpose.load_files(files)
-            sub_products += sub_products_t
-            sub_parts += sub_parts_t
+            data_subassemblies_t = PLM_BOM_transpose.load_files(files)
             data_subassemblies.update(data_subassemblies_t)
-            sub_products = list(set(sub_products))
-            sub_parts = list(set(sub_parts))
         except Exception as e:
             print(e)
             sg.Popup('Wrong file', keep_on_top=True)
@@ -335,21 +342,21 @@ while True:
     elif event == "Expand for all":
         if not table_clicked_right:
             try:
-                expand(window['-TABLE_LEFT-'].get()[table_clicked_left], products)
+                expand(window['-TABLE_LEFT-'].get()[table_clicked_left], "all_products")
                 compare("input")
             except Exception as e:
                 print(e)
                 continue
         elif not table_clicked_left:
             try:
-                expand(window['-TABLE_RIGHT-'].get()[table_clicked_right], products)
+                expand(window['-TABLE_RIGHT-'].get()[table_clicked_right], "all_products")
                 compare("input")
             except:
                 continue
 
     elif event == 'Expand all subassemblies':
         try:
-            expand(sub_products, products)
+            expand("all_subassemblies", "all_products")
             compare("input")
         except Exception as e:
             print(e)
@@ -375,7 +382,7 @@ while True:
         window['-TABLE_RIGHT-'].update([])
 
     elif event == 'Clear data':
-        products, parts, sub_products, sub_parts = [], [], [], []
+        data, data_subassemblies = {}, {}
         window['-IN_LEFT-'].update('')
         window['-BOX-CONTAINER_LEFT-'].update(visible=False)
         window['-IN_RIGHT-'].update('')
@@ -399,7 +406,7 @@ while True:
             input_text_left = text
         prediction_list_left = []
         if text:
-            prediction_list_left = [item for item in products if item.startswith(text)]
+            prediction_list_left = [item for item in data.keys() if item.startswith(text)]
 
         list_element_left.update(values=prediction_list_left)
         sel_item_left = 0
@@ -419,7 +426,7 @@ while True:
             input_text_right = text
         prediction_list_right = []
         if text:
-            prediction_list_right = [item for item in products if item.startswith(text)]
+            prediction_list_right = [item for item in data.keys() if item.startswith(text)]
 
         list_element_right.update(values=prediction_list_right)
         sel_item_right = 0
