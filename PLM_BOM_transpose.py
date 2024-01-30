14cb2443c88c402708647ae6fd2624a2e0fb0111
dczarny10 <dczarny10@gmail.com>
2024-01-30 15:31:56 +0100

    Added option to save data structure to Excel file

--- PLM_BOM_transpose.py
+++ PLM_BOM_transpose.py
@@ -32,7 +32,7 @@ def load_files(paths):
                         index = i
                         break
                 if index == -1:
-                    data[product_name].append(tuple(map(str, (q[1], q[2], q[3]))))
+                    data[product_name].append(tuple(map(str, (q[1], q[2], q[3].strip()))))
                 else:
                     try:
                         data[product_name][index] = (data[product_name][index][0], str(int(data[product_name][index][1])+int(q[2])), data[product_name][index][2])
@@ -49,17 +49,18 @@ def load_files(paths):
             products = [i for i in data_PLM[0][3:]] #create products numbers list
             data_PLM = data_PLM[1:] #remove headlines in excel file
             for count, i in enumerate(products):
+                i = i.strip()
                 data.setdefault(i, [])
                 for j in data_PLM:
                     if j[3+count] != "0": #if part belong to this product
-                        r = re.findall(pattern, j[0]) #find part number #python regular expression because part numbers are sadly not standarized now
+                        r = re.findall(pattern, j[0].strip()) #find part number #python regular expression because part numbers are sadly not standarized
                         index = -1
                         for k, obj in enumerate(data[i]): #check if part is already under this product BOM
                             if obj[0] == r[0][0]:
                                 index = k
                                 break
                         if index == -1:
-                            data[i].append((r[0][0], j[3 + count].split(".")[0], r[0][1]))
+                            data[i].append((r[0][0], j[3 + count].split(".")[0], r[0][1].strip()))
                         else:
                             try:
                                 data[i][index] = (data[i][index][0], str(int(data[i][index][1]) + int(j[3 + count].split(".")[0])), data[i][index][2])
@@ -71,10 +72,10 @@ def load_files(paths):
             data_PLM = data_PLM[1:]  # remove headlines in excel file
             products = set([i[4] for i in data_PLM if i[4] is not None])
             for count, i in enumerate(products):
-                product_name = i + "_MMC"
+                product_name = i.strip() + "_MMC"
                 data.setdefault(product_name, [])
                 for j in data_PLM:
-                    if j[4] == i: #if part belong to this product
+                    if j[4].strip() == i.strip(): #if part belong to this product
                         r = j[1].strip()
                         index = -1
                         for k, obj in enumerate(data[product_name]): #check if part is already under this product BOM
@@ -82,7 +83,7 @@ def load_files(paths):
                                 index = k
                                 break
                         if index == -1:
-                            data[product_name].append((r, str(j[2]), j[3]))
+                            data[product_name].append((r, str(j[2]), j[3].strip()))
                         else:
                             if data[product_name][index][1] == ' ' or j[2] == ' ':
                                 data[product_name][index] = (data[product_name][index][0], ' ', data[product_name][index][2])
@@ -97,10 +98,10 @@ def load_files(paths):
             data_PLM = data_PLM[1:]  # remove headlines in excel file
             products = set([i[4] for i in data_PLM if i[4] is not None])
             for count, i in enumerate(products):
-                product_name = i + "_PDF"
+                product_name = i.strip() + "_PDF"
                 data.setdefault(product_name, [])
                 for j in data_PLM:
-                    if j[4] == i: #if part belong to this product
+                    if j[4].strip() == i.strip(): #if part belong to this product
                         r = j[1].strip()
                         index = -1
                         for k, obj in enumerate(data[product_name]): #check if part is already under this product BOM
@@ -108,7 +109,7 @@ def load_files(paths):
                                 index = k
                                 break
                         if index == -1:
-                            data[product_name].append((r, str(j[2]), j[3]))
+                            data[product_name].append((r, str(j[2]), j[3].strip()))
                         else:
                             try:
                                 data[product_name][index] = (data[product_name][index][0], str(int(data[product_name][index][1]) + int(j[2])), data[product_name][index][2])
